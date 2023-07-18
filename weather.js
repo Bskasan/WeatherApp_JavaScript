83758659f285f1d4c7632bfbab4d611101edfaf7
Bskasan <b.kasan@yandex.com>
2023-07-18 23:17:41 +0300

    encryption of api key done

--- weather.js
+++ weather.js
@@ -1,2 +1,14 @@
 const form = document.querySelector("form"); // Faster way
 const input = document.querySelector("form input");
+const msgSpan = form.querySelector(".msg");
+const listCities = document.querySelector(".container .cities");
+
+localStorage.setItem(
+  "apiKey",
+  // ? Encrypted Our Api to save it local storage.
+  EncryptStringAES("c329c3b96528c0c0f92577a6850e5a5e")
+);
+
+// ! -------------- INTERVIEW QUESTION -------------- //
+// ? .class .class vs. .class.class
+// ! -------------- ------------------ -------------- //
